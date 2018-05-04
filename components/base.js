993e797b335a3663bd64561b80afa0de459fa1d2
Yann Gouffon <github@yago-art.ch>
2018-05-04 16:09:47 +0200

    Update 2018-05-04T14:09:43.393Z

--- components/base.js
+++ components/base.js
@@ -1,17 +1,17 @@
-/* globals jQuery */
+/* globals jQuery, Tablesaw */
 
 import upload from './atoms/upload/upload';
 import datebpicker from './molecules/datepicker/datepicker';
+import datepickerEvent from './molecules/datepicker-event/datepicker-event';
 import socialShare from './organisms/social/social-share';
-import footer from './organisms/footer/footer';
 import svgIcons from '../icons/svg-icons';
 
 svgIcons(); // Must run as soon as possible
 
 const init = () => {
   upload();
-  footer();
   datebpicker();
+  datepickerEvent();
   socialShare();
   Tablesaw.init();
 
